bcc0ad9485e20efecbfd12ac01406f0e6a8ac679
Juan Trejo <jtrejo4545@gmail.com>
2021-04-23 13:52:53 -0500

    sign up

--- src/Components/login/SignUpComponent.jsx
+++ src/Components/login/SignUpComponent.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import UserDataService from '../../service/UserDataService';
 
 class SignUpComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            password: null,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
     componentDidMount() {
         document.body.style = 'background: #F7F7F7;';
     }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleSubmit() {
+        let user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+
+        UserDataService.addUser(user)
+            .then(alert("created user"))
+            .then(this.props.history.push(`/`));
+    }
+
     render() {
         return (
             <div className="container clear-top" id="main">
@@ -13,12 +40,27 @@ class SignUpComponent extends Component {
                         <div className="jumbotron" style={{ textAlign: "center", backgroundColor: "white" }}>
                             <h1 style={{ color: "Blue" }}>Sign Up</h1>
                             <br />
-                            <form >
-                                Username <input type="text" />
-                                <br />
-                    Password <input type="password" />
-                                <br />
-                                <button type="submit">Sign up</button>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input className="form-control"
+                                        type="text"
+                                        name="username"
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input className="form-control"
+                                        type="password"
+                                        name="password"
+                                        onChange={this.handleChange}
+                                    />
+
+                                </div>
+                                <div className="form-group">
+                                    <button type="submit">Sign up</button>
+                                </div>
                             </form>
                         </div>
                     </div>
--- src/service/UserDataService.js
+++ src/service/UserDataService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 class UserDataService {
-
+    addUser(user) {
+        return axios.post(`http://localhost:8080/user/addUser`, user);
+    }
 }
 export default new UserDataService()
